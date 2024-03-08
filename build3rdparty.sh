ef4605657a94de8429bff6e7f8db901cd4470b11
Yinan Qin <39023210+ganyuanzhen@users.noreply.github.com>
2024-03-09 00:06:53 +0800

    Update build3rdparty.sh

--- build3rdparty.sh
+++ build3rdparty.sh
@@ -10,12 +10,12 @@ fi
 #build opencv
 if [ ! -f "3rdparty/opencv-4.5.4/build/install/lib/libopencv_world.a" ]; then
 cd 3rdparty/opencv-4.5.4
-patch -p1 --batch -i ../opencv-mobile/opencv-4.5.4-no-zlib.patch
+# patch -p1 --batch -i ../opencv-mobile/opencv-4.5.4-no-zlib.patch
 truncate -s 0 cmake/OpenCVFindLibsGrfmt.cmake
 rm -rf modules/gapi
 rm -rf modules/highgui
 cp -r ../opencv-mobile/highgui modules/
-patch -p1 --batch -i ../opencv-mobile/opencv-4.5.4-no-rtti.patch
+# patch -p1 --batch -i ../opencv-mobile/opencv-4.5.4-no-rtti.patch
 rm -rf build && mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release `cat ../../opencv-mobile/opencv4_cmake_options.txt` -DBUILD_opencv_world=ON ..
 make -j$JOBS && make install
